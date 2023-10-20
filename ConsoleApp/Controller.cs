cb57d5e3bed8158cdbdcc62bf5d69ab2296aace8
jungie gerez <gerezjungie393@yahoo.com>
2023-10-20 15:59:21 +0800

    Add project files.

---
+++ ConsoleApp/Controller.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public class Controller : Persons
+    {
+        public List<Persons> People = new List<Persons>();
+        public void CreatePerson()
+        {
+            People.Add(new Persons { PersonId = "1", PersonName = "t", PersonDescription = "z" });
+            People.Add(new Persons { PersonId = "2", PersonName = "a", PersonDescription = "x" });
+            People.Add(new Persons { PersonId = "3", PersonName = "d", PersonDescription = "c" });
+        }
+
+        public List<Persons> GetAllPeople()
+        {
+            return People;
+        }
+
+        public void setPersonID(string id, string replaceID)
+        {
+            Persons personWithId = People.FirstOrDefault(person => person.PersonId == id);
+
+            if (personWithId != null)
+            {
+                personWithId.PersonId = replaceID;
+            }
+        }
+
+        public void CreatePerson(Persons pr)
+        {
+            People.Add(pr);
+        }
+
+        public void DeletePerson(string ID)
+        {
+            Persons personToDelete = People.FirstOrDefault(person => person.PersonId == ID);
+
+            if (personToDelete != null)
+            {
+                People.Remove(personToDelete);
+            }
+        }
+    }
+}
---
+++ ConsoleApp/View.cs
@@ -0,0 +1,65 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp;
+
+    Controller ps  = new Controller();
+
+    ps.CreatePerson();
+
+
+string input = "";
+    while(!input.ToLower().Equals("x"))
+    {
+        Console.WriteLine("1: Create");
+        Console.WriteLine("2: Retrieve");
+        Console.WriteLine("3: Update");
+        Console.WriteLine("4: Delete");
+        Console.WriteLine("X: Exit");
+        input = Console.ReadLine();
+
+        switch (input)
+        {
+            case "1":
+            string ID = Console.ReadLine();
+            string NAME = Console.ReadLine();
+            string DESC = Console.ReadLine();
+
+            ps.CreatePerson(new Persons { PersonId = ID, PersonName = NAME, PersonDescription = DESC });
+            Console.WriteLine("Successfully Added");
+                break;
+            case "2":
+                foreach (var i in ps.GetAllPeople())
+                {
+                    Console.WriteLine($"ID   : {i.PersonId}");
+                    Console.WriteLine($"Name : {i.PersonName}");
+                    Console.WriteLine($"Desc : {i.PersonDescription}");
+                    Console.WriteLine("--------------------");
+                }
+            break;
+            case "3":
+            Console.WriteLine("What ID?");
+            string FindID = Console.ReadLine();
+
+            Console.WriteLine("Replacement ID");
+            string ReplacementID = Console.ReadLine();
+
+                ps.setPersonID(FindID, ReplacementID);
+                Console.WriteLine("Success Update ID");
+                break;
+        case "4":
+            string DeleteID = Console.ReadLine();
+
+            ps.DeletePerson(DeleteID);
+            break;
+
+        default:
+                Console.WriteLine();
+                break;
+        }
+
+    
+    }
+
+
+Console.WriteLine("Thank you for the information!");
+
+
